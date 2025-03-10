21f347512e6f589330389306b07310039441416c
Amartya Kumar <21ucs018@lnmiit.ac.in>
2025-03-10 20:39:20 +0530

    Added Backend Folder

---
+++ Full Stack Easy Project/Backend/index.js
@@ -0,0 +1,11 @@
+// basic express boiler plate code
+const express = require('express')
+
+const app = express()
+
+app.use(express.json());
+
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000..");
+})
